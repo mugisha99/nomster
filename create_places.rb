90f753288623a4f0ba38e3e522bc35c380da942e
mugisha <mugishaj@outlook.com>
2015-09-25 06:57:21 +0000

    add file containing some dummy places

---
+++ create_places.rb
@@ -0,0 +1,13 @@
+Place.create(name:"Bwindi impenetrable national park", description:"mist-covered hillsides",
+               address:"southwestern Uganda" )
+Place.create(name:"Kibale national park", description:"contains one of the loveliest and most varied tracts of tropical forest in Uganda",
+	      address:"Kibale Fort-Portal" )
+Place.create(name:"Kidepo valley national park", description:"lies in the rugged, semi arid valleys between Uganda's borders with Sudan and Kenya",
+	address:"Karamoja region" )
+Place.create(name:"Lake Mburo national park", description:"a compact gem located conveniently close to highway that connects Kampala to the parks of Western Uganda
+		", address:"Karamoja")
+Place.create(name:"Mgahinga gorilla national park", address:"far away", description:"sits high in the clouds at an altitutde of between 2227m and 4127m")
+Place.create(name: "Mount Elgon national park", address:"Kenya-Uganda border", description:"Has the largest volcanic base in the world. Oldest and largest solitary park")
+Place.create(name:"Murchison falls national park", address:"Nothern end of Albertine Rift Valley", description:"Where the sweeping Bunyoro escarpment tumbles into vast, palm-dotted savanna")
+Place.create(name:"Queen Elizabeth national park", address:"Beautiful Area", description:"Uganda's most popular tourist destination. The park's diverse ecosystem...")
+Place.create(name:"Semuliki national park", address:"Across the floor of the Semliki Valley", description:"Found in the western side of the Rwenzori")
