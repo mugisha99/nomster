ce39fce10da560e4fe79a3d448ec89140e7c3e17
mugisha <mugishaj@outlook.com>
2015-10-19 13:37:25 +0000

    integrate a functioning image uploader

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,7 +7,7 @@ class PhotosController < ApplicationController
 
 	private
 	def photo_params
-		params.require(:photo).permit(:caption)
+		params.require(:photo).permit(:caption, :picture)
 
 	end
 
